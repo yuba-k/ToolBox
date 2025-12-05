aa46d193043223bac813189f88717415cbe6b501
yuba-k <160005925+yuba-k@users.noreply.github.com>
2025-12-05 16:11:09 +0900

    Set camera controls for exposure and metering

--- picamera-scripts/capture.py
+++ picamera-scripts/capture.py
@@ -1,4 +1,5 @@
 from picamera2 import Picamera2
+from libcamera import controls
 import cv2
 import numpy as np
 import datetime
@@ -9,6 +10,13 @@ class Camera():
     def __init__(self):
         try:
             self.picam = Picamera2()
+            self.picam.set_controls(
+                {
+                    "AeConstraintMode":controls.AeConstraintModeEnum.Highlight, #露光モード：ハイライト
+                    "AeExposureMode": controls.AeExposureModeEnum.Normal,       # 通常露光
+                    "AeMeteringMode": controls.AeMeteringModeEnum.Matrix        # 全体を考慮した測光
+                }
+            )
         except Exception as e:
             print("カメラの初期化中にエラーが発生しました")
             raise
