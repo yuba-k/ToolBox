517ef9f776d79b2621d2e6f4d38400e47e7ea56c
yuba-k <160005925+yuba-k@users.noreply.github.com>
2025-12-14 20:27:07 +0900

    関数分離

--- parse_csv/plot_latlon.py
+++ parse_csv/plot_latlon.py
@@ -4,46 +4,57 @@ import pandas as pd
 import numpy as np
 import os
 
-def type_goal(i):
-    lon,lat = map(float,i.split(","))
-    return lon,lat
+def type_goal(goal_coordinates):
+    try:
+        lon,lat = map(float,goal_coordinates.split(","))
+        return lon,lat
+    except ValueError:
+        raise argparse.ArgumentTypeError("\ngoal座標が正しい形式ではありません\nlon,latの形式で入力してください")
+
+def init():
+    """init
+    グラフ描画の初期設定
+    """
+    plt.axis("equal")
+    plt.title("Route History")
+    plt.xlabel("lon")
+    plt.ylabel("lat")
+
+def draw_scatter(data,filename,goal_coordinates,saveFlag,displayFlag):
+    plt.scatter(data["lon"],data["lat"],color="green")
+    if goal_coordinates is not None:
+        plt.scatter(goal_coordinates[0],goal_coordinates[1],color="red",marker="*")
+    plt.plot(data["lon"],data["lat"],color="green")
+    if saveFlag:
+        saved_name = os.path.split(filename.name)[1].split('.')[0]
+        print(f"suscessful to save \"{saved_name}.png\"")
+        plt.savefig(saved_name+".png")
+    if displayFlag:
+        plt.show()
 
 def main():
     parser = argparse.ArgumentParser(description="csvファイルに保存された位置情報データから線付き散布図(移動経路履歴図)を作成する")
 
-    parser.add_argument("-o","--open",type=str,help="解析対象のファイルのパス指定",required=True)
+    parser.add_argument("-o","--open",type=argparse.FileType("r"),help="解析対象のファイルのパス指定",required=True)
     parser.add_argument("-s","--save",action="store_true",help="生成した散布図の保存の有無")
     parser.add_argument("-g","--goal",type=type_goal,help="ゴール座標(lon,lat)で与える．作成される散布図にゴール座標が表示される")
     parser.add_argument("-d","--display",action="store_true",help="グラフ表示の有無")
 
     args = parser.parse_args()
-
+    init()
     try:
         df = pd.read_csv(args.open,header=None,names=["lon","lat"],dtype=float)
     except FileNotFoundError:
         print(f"{args.open}を開けませんでした．\nファイル名,パスが正しいことを確認してください")
         return
-        
+    df = df.dropna(how="any")   
     if df.empty:
         print(f"{args.open}は空です．")
         return
-    df = df.dropna(how="any")
-    plt.axis("equal")
-    plt.scatter(df["lon"],df["lat"],color="green")
-    if args.goal is not None:
-        plt.scatter(args.goal[0],args.goal[1],color="red",marker="*")
-    plt.plot(df["lon"],df["lat"],color="green")
-    plt.title(args.open)
-    plt.xlabel("lon")
-    plt.ylabel("lat")
-    plt.xticks(np.arange(min(df["lon"]),max(df["lon"]),(max(df["lon"])-min(df["lon"]))/5))
-    plt.yticks(np.arange(min(df["lat"]),max(df["lat"]),(max(df["lat"])-min(df["lat"]))/5))
-    if args.save:
-        saved_name = os.path.split(args.open)[1].split('.')[0]
-        print(f"suscessful to save \"{saved_name}.png\"")
-        plt.savefig(saved_name+".png")
-    if args.display:
-        plt.show()
+    elif len(df["lon"]) == 1:
+        print("データが1セットしかありません\n移動経路図作成には2セット以上が必要です")
+        return
+    draw_scatter(data=df,filename=args.open,goal_coordinates=args.goal,saveFlag=args.save,displayFlag=args.display)
     
 if __name__ == "__main__":
     main()
