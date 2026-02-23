5259deb25b7c2551637da33f4b1a90740cfb8395
yuba-k <160005925+yuba-k@users.noreply.github.com>
2026-02-23 20:41:46 +0900

    IMU単純積分

---
+++ 6-axis-sensor/imu.py
@@ -0,0 +1,53 @@
+"""
+単純積分
+"""
+import board
+from adafruit_lsm6ds.lsm6ds33 import LSM6DS33
+from adafruit_lsm6ds import Rate, GyroRange
+import time
+import math
+import threading
+
+class GYRO():
+    def __init__(self):
+        i2c = board.I2C()
+        self.sensor = LSM6DS33(i2c)
+
+        self.sensor.gyro_data_rate = Rate.RATE_416_HZ
+        self.sensor.gyro_range = GyroRange.RANGE_250_DPS
+
+    def _load(self):
+        old = time.perf_counter()
+        while self.running:
+            now = time.perf_counter()
+            dt = now - old
+            gyro_z = math.degrees(self.sensor.gyro[2])
+            self.angle_z += gyro_z * dt
+            old = now
+            time.sleep(1/416)
+
+    def start(self):
+        self.angle_z = 0.0
+        self.running = True
+        self.threadLoad = threading.Thread(target = self._load, daemon = True)
+        self.threadLoad.start()
+
+    def stop(self):
+        self.running = False
+        self.threadLoad.join()
+
+    def read(self):
+        return self.angle_z
+
+def main():
+    try:
+        gyro = GYRO()
+        gyro.start()
+        while True:
+            print(gyro.read())
+    except Exception:
+        if gyro is not None:
+            gyro.stop()
+
+if __name__ == "__main__":
+    main()
